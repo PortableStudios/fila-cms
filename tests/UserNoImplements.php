fd6e291c0e268a4442e1cf97ea2fe8a0210a30c6
Kath Young <kath.young@portable.com.au>
2024-03-08 09:27:43 +1030

    Add a User class without interfaces and traits

---
+++ tests/UserNoImplements.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Portable\FilaCms\Tests;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class UserNoImplements
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+
+
+    public function canAccessFilament(): bool
+    {
+        // This is required on Front and Back end.  Add more specific controls with authenticate middleware.
+        return true;
+    }
+
+
+
+    public function canAccessPanel($panel): bool
+    {
+        // This is required on Front and Back end.  Add more specific controls with authenticate middleware.
+        return true;
+    }
+
+}
