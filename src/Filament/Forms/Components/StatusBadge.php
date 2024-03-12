7c98c5076273035a464b15c96325678fd65ed382
Nick Rogers <nick.rogers@portable.com.au>
2024-03-13 01:36:57 +1030

    style: fix formatting test: add update_sets_slug chore: swap to .editorconfig

--- src/Filament/Forms/Components/StatusBadge.php
+++ src/Filament/Forms/Components/StatusBadge.php
@@ -5,15 +5,11 @@ namespace Portable\FilaCms\Filament\Forms\Components;
 use Filament\Forms\Components\Field;
 use Filament\Support\Concerns\HasBadge;
 use Filament\Support\Concerns\HasColor;
-use Illuminate\Database\Eloquent\Model;
 
 class StatusBadge extends Field
 {
-
     use HasBadge;
     use HasColor;
-
-    // protected string $view = 'forms.components.status-badge';
 
     public const BADGE_VIEW = 'forms.components.status-badge';
 }
--- tests/Unit/PageEventsTest.php
+++ tests/Unit/PageEventsTest.php
@@ -4,6 +4,7 @@ namespace Portable\FilaCms\Tests\Unit;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Str;
 use Portable\FilaCms\Models\Author;
 use Portable\FilaCms\Models\Page;
 use Portable\FilaCms\Tests\TestCase;
@@ -52,5 +53,29 @@ class PageEventsTest extends TestCase
         $page->is_draft = 0;
         $page->save();
         $this->assertNotNull($page->publish_at);
+    }
+
+    public function test_update_sets_slug()
+    {
+        $author = Author::create([
+            'first_name' => $this->faker->firstName,
+            'last_name' => $this->faker->lastName,
+            'is_individual' => 1
+        ]);
+
+        $data = [
+            'title'     => $this->faker->words(15, true),
+            'is_draft'  => 0,
+            'contents'  => $this->faker->words($this->faker->numberBetween(50, 150), true),
+            'author_Id' => $author->id,
+        ];
+        $page = Page::create($data);
+        $this->assertNotNull($page->slug);
+        $newTitle = $this->faker->words(15, true);
+        $page->title = $newTitle;
+        $page->slug = null;
+        $page->save();
+        $this->assertModelExists($page);
+        $this->assertDatabaseHas('pages', ['slug' => Str::slug($newTitle)]);
     }
 }
