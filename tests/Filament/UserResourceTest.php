1766dfb0872b3990eb556e7e9c17ee68b65220c8
Nick Rogers <nick.rogers@portable.com.au>
2024-03-22 17:26:36 +1030

    feat: user create and edit form test: user tests

--- tests/Filament/UserResourceTest.php
+++ tests/Filament/UserResourceTest.php
@@ -2,15 +2,20 @@
 
 namespace Portable\FilaCms\Tests\Filament;
 
-use Portable\FilaCms\Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Portable\FilaCms\Filament\Resources\UserResource;
-use Spatie\Permission\Models\Role;
+use Illuminate\Foundation\Testing\WithFaker;
 use Livewire\Livewire;
+use Portable\FilaCms\Filament\Resources\UserResource as TargetResource;
+use Portable\FilaCms\Filament\Resources\UserResource;
+
+use Portable\FilaCms\Tests\User as TargetModel;
+use Portable\FilaCms\Tests\TestCase;
+use Spatie\Permission\Models\Role;
 
 class UserResourceTest extends TestCase
 {
     use RefreshDatabase;
+    use WithFaker;
 
     protected function setUp(): void
     {
@@ -45,5 +50,83 @@ class UserResourceTest extends TestCase
         }
 
         Livewire::test(UserResource\Pages\ListUsers::class)->assertCanSeeTableRecords($users);
+    }
+
+    public function test_can_create_record(): void
+    {
+        Livewire::test(TargetResource\Pages\CreateUser::class)
+            ->fillForm($this->generateModel(true))
+            ->call('create')
+            ->assertHasNoFormErrors();
+
+        Livewire::test(TargetResource\Pages\CreateUser::class)
+            ->fillForm([])
+            ->call('create')
+            ->assertHasFormErrors([
+                'name' => 'required',
+                'email' => 'required',
+            ]);
+    }
+
+    public function test_can_render_edit_page(): void
+    {
+        $data = $this->generateModel();
+
+        $this->get(TargetResource::getUrl('edit', ['record' => $data]))->assertSuccessful();
+    }
+
+    public function test_can_retrieve_edit_data(): void
+    {
+        $data = $this->generateModel();
+
+        Livewire::test(
+            TargetResource\Pages\EditUser::class,
+            ['record' => $data->getRouteKey()]
+        )
+            ->assertFormSet([
+                'name' => $data->name,
+            ]);
+    }
+
+    public function test_can_save_form(): void
+    {
+        $data = $this->generateModel();
+
+        $new = TargetModel::make($this->generateModel(true));
+
+        Livewire::test(TargetResource\Pages\EditUser::class, [
+            'record' => $data->getRoutekey(),
+        ])
+            ->fillForm([
+                'name' => $new->name,
+                'email' => $new->email,
+                'password' => $new->password,
+                'roles' => $new->roles,
+            ])
+            ->call('save')
+            ->assertHasNoFormErrors();
+        $updatedTime = now();
+
+        $data->refresh();
+        $this->assertEquals($data->name, $new->name);
+        $this->assertEquals($data->updated_at->format('Y-m-d H:i'), $updatedTime->format('Y-m-d H:i'));
+    }
+
+    public function generateModel($raw = false): TargetModel|array
+    {
+        $adminRole = Role::where('name', 'Admin')->first();
+
+        $data = [
+            'name' => $this->faker->name,
+            'email' => $this->faker->email,
+            'password' => $this->faker->password,
+            'roles' => [$adminRole->id],
+        ];
+
+        if ($raw) {
+            return $data;
+        }
+
+        return TargetModel::create($data);
     }
 }
