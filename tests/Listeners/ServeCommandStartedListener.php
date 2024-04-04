adcbbb268131ff3dcc8bb2cdfefa124991ab91b6
Kath Young <kath.young@portable.com.au>
2024-04-04 17:34:37 +1030

    Enable workbench serve

---
+++ tests/Listeners/ServeCommandStartedListener.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Portable\FilaCms\Tests\Listeners;
+
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Hash;
+
+class ServeCommandStartedListener
+{
+    public function handle(): void
+    {
+        Artisan::call('package:create-sqlite-db');
+        Artisan::call('migrate:fresh');
+
+        // Setup the FilaCMS install
+        // remove config files
+        File::delete(config_path('fila-cms.php'));
+        File::delete(config_path('filament-tiptap-editor.php'));
+
+        // remove migrations from revisionable
+        File::delete(database_path('migrations/2013_04_09_062329_create_revisions_table.php'));
+
+        // remove filament theme
+        File::delete(resource_path('css/filament/admin/tailwind.config.js'));
+        File::delete(resource_path('css/filament/admin/theme.css'));
+
+        Artisan::call('fila-cms:install', ['--publish-config' => true,'--run-migrations' => true,'--add-user-traits' => true]);
+
+        // Ensure there's an admin user
+        $admin = User::where('email', 'admin@test.com')->first();
+        if(!$admin) {
+            $admin = new User();
+        }
+        $admin->email = 'admin@test.com';
+        $admin->password = Hash::make('password');
+        $admin->name = 'Admin';
+        $admin->save();
+        $admin->assignRole('Admin');
+    }
+}
