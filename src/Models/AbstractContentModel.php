e4668784c7e23c10300f39c7dbf48ab95e22e6ab
Kath Young <kath.young@portable.com.au>
2024-04-09 07:34:14 +0930

    Pipeline debugging

--- src/Models/AbstractContentModel.php
+++ src/Models/AbstractContentModel.php
@@ -90,6 +90,9 @@ abstract class AbstractContentModel extends Model
          */
         $attributes = $refreshedModel->getSnapshotAttributes();
 
+        echo "Attributes";
+        print_r($attributes);
+
         return Version::createForModel($refreshedModel, $attributes, $refreshedModel->updated_at);
     }
 
