8587de2ae9b09ba6617e0a5576aaca5154ec16fc
Nick Rogers <nick.rogers@portable.com.au>
2024-04-08 11:24:03 +0930

    wip: filament screens for managing SEO and OG data

--- src/Filament/Resources/AbstractContentResource.php
+++ src/Filament/Resources/AbstractContentResource.php
@@ -2,7 +2,6 @@
 
 namespace Portable\FilaCms\Filament\Resources;
 
-use Filament\Forms\Form;
 use Filament\Forms\Components\CheckboxList;
 use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Fieldset;
@@ -11,19 +10,23 @@ use Filament\Forms\Components\Placeholder;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Tabs;
+use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Toggle;
 use Filament\Forms\Components\View;
+use Filament\Forms\Form;
+use Filament\Forms\Get;
 use Filament\Tables;
-use Filament\Tables\Table;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Filters\TernaryFilter;
 
-use FilamentTiptapEditor\TiptapEditor;
+use Filament\Tables\Table;
 use FilamentTiptapEditor\Enums\TiptapOutput;
+use FilamentTiptapEditor\TiptapEditor;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\HtmlString;
 use Portable\FilaCms\Filament\Forms\Components\StatusBadge;
 use Portable\FilaCms\Filament\Resources\AbstractContentResource\Pages;
 use Portable\FilaCms\Filament\Resources\AbstractContentResource\RelationManagers;
@@ -32,7 +35,6 @@ use Portable\FilaCms\Models\Author;
 use Portable\FilaCms\Models\Page;
 use Portable\FilaCms\Models\Scopes\PublishedScope;
 use Portable\FilaCms\Models\TaxonomyResource;
-use RalphJSmit\Filament\SEO\SEO;
 use Str;
 
 class AbstractContentResource extends AbstractResource
@@ -62,18 +64,21 @@ class AbstractContentResource extends AbstractResource
                         ->tabs([
                             Tabs\Tab::make('Content')
                                 ->schema([
-
                                     TextInput::make('title')
                                         ->columnSpanFull()
                                         ->required(),
                                     static::tiptapEditor()->output(\FilamentTiptapEditor\Enums\TiptapOutput::Json),
-                                    SEO::make(['description']),
                                 ]),
                             Tabs\Tab::make('Taxonomies')
                                 ->schema([
                                     ...static::getTaxonomyFields(),
                                 ]),
-                        ]),
+                            Tabs\Tab::make('SEO')
+                                ->schema([
+                                    ...static::getSeoFields(),
+                                ]),
+                        ])
+                        ->persistTabInQueryString()
                 ])
                 ->columnSpan(2),
             Group::make()
@@ -143,6 +148,131 @@ class AbstractContentResource extends AbstractResource
         });
 
         return $taxonomyFields;
+    }
+
+    public static function getSeoFields(): array
+    {
+        $seoFields = [
+            Section::make('Search Engine Optimisation')
+                ->compact()
+                ->description('SEO metadata is automatically generated from your content. Override these fields to customise how your content appears in search engine results.')
+                ->columns(12)
+                ->schema([
+                    Toggle::make('override_seo_title')
+                        ->columnSpan(3)
+                        ->inline(false)
+                        ->label('Override Title')
+                        ->live(),
+                    TextInput::make('seo_title')
+                        ->columnSpan(9)
+                        ->label('Title')
+                        ->hintColor('info')
+                        ->hintIcon('heroicon-m-question-mark-circle', tooltip: 'Make sure your title is explicit and contains your most important keywords. Each page should have a unique title.')
+                        ->placeholder(fn (Get $get): string => $get('title') ?? '')
+                        ->disabled(fn (Get $get): bool => !$get('override_seo_title')),
+                    Toggle::make('override_seo_description')
+                        ->columnSpan(3)
+                        ->inline(false)
+                        ->label('Override Description')
+                        ->live(),
+                    Textarea::make('seo_description')
+                        ->columnSpan(9)
+                        ->maxLength(155)
+                        ->label('Description')
+                        ->hintColor('info')
+                        ->hintIcon('heroicon-m-question-mark-circle', tooltip: 'SEO descriptions allow you to influence how your web pages are described and displayed in search results. Ensure that all of your web pages have a unique meta description that is explicit and contains your most important keywords.')
+                        ->helperText(function (?string $state): HtmlString {
+                            $length = strlen($state);
+                            $lengthStatus = match (true) {
+                                $length > 160 => 'danger',
+                                $length === 160 => 'success',
+                                $length >= 140 => 'warning',
+                                $length >= 80 => 'success',
+                                default => 'gray',
+                            };
+                            return new HtmlString(
+                                Str::of("<span style=\"color: rgba(var(--{$lengthStatus}-500),var(--tw-text-opacity))\">" . strlen($state) . '</span>')
+                                    ->append(' / ')
+                                    ->append(160 . ' ')
+                                    ->append('characters')
+                            );
+                        })
+                        ->maxLength(160)
+                        ->reactive()
+                        ->placeholder(fn (Get $get): string => $get('summary') ?? '')
+                        ->disabled(fn (Get $get): bool => !$get('override_seo_description')),
+                ]),
+            Section::make('Open Graph')
+                ->compact()
+                ->description('Open Graph metadata is automatically generated from your content. Override these fields to customise how your content appears when a user posts a link on Facebook.')
+                ->columns(12)
+                ->schema([
+                        Toggle::make('override_og_title')
+                            ->columnSpan(3)
+                            ->inline(false)
+                            ->label('Override Title')
+                            ->live(),
+                        TextInput::make('og_title')
+                            ->columnSpan(9)
+                            ->label('Title')
+                            ->hintColor('info')
+                            ->hintIcon('heroicon-m-question-mark-circle', tooltip: 'Specifies the title of your content when users share this page on social media. A succinct and accurate title will grab the attention of users and encourage them to click on your content.')
+                            ->placeholder(fn (Get $get): string => $get('title') ?? '')
+                            ->disabled(fn (Get $get): bool => !$get('override_og_title')),
+                        Toggle::make('override_og_description')
+                            ->columnSpan(3)
+                            ->inline(false)
+                            ->label('Override Description')
+                            ->live(),
+                        Textarea::make('og_description')
+                            ->columnSpan(9)
+                            ->maxLength(155)
+                            ->label('Description')
+                            ->hintColor('info')
+                            ->hintIcon('heroicon-m-question-mark-circle', tooltip: 'This is shown below the Title when users share this page on social media. Provide a summary of the content and purpose of the page.')
+                            ->placeholder(fn (Get $get): string => $get('summary') ?? '')
+                            ->disabled(fn (Get $get): bool => !$get('override_og_description')),
+                        Select::make('og_type')
+                            ->columnStart(4)
+                            ->columnSpan(9)
+                            ->label('Type')
+                            ->hintColor('info')
+                            ->hintIcon('heroicon-m-question-mark-circle', tooltip: 'The type should be the most specific type that describes your content. For example, if your content is a blog post, you should set the type to article.')
+                            ->options([
+                                'book' => 'Book',
+                                'business.business' => 'Business',
+                                'music.album' => 'Music Album',
+                                'music.song' => 'Music Song',
+                                'place' => 'Place',
+                                'product' => 'Product',
+                                'profile' => 'Profile',
+                                'restaurant.restaurant' => 'Restaurant',
+                                'video.other' => 'Video',
+                                'website' => 'Website',
+                            ])
+                            ->default('website')
+                            ->selectablePlaceholder(false),
+                ]),
+            Section::make('Robots')
+                ->compact()
+                ->description(str('If you do not want this page to be indexed by search engines, you can set the robots meta tag to **No Index**. **No Follow** will also prevent search engines from following links on this page.')->inlineMarkdown()->toHtmlString())
+                ->columns(12)
+                ->schema([
+                        Select::make('robots')
+                            ->columnSpanFull()
+                            ->label('')
+                            ->options([
+                                'index, follow' => 'Index, Follow',
+                                'noindex, follow' => 'No Index, Follow',
+                                'index, nofollow' => 'Index, No Follow',
+                                'noindex, nofollow' => 'No Index, No Follow',
+                            ])
+                            ->default('index, follow')
+                            ->selectablePlaceholder(false)
+                ]),
+        ];
+
+        return $seoFields;
     }
 
     public static function tiptapEditor($name = 'contents'): TiptapEditor
