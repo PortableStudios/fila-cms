d88b411cb3d093b69e0dbdf93163f7b711503be4
Kath Young <kath.young@portable.com.au>
2024-05-08 21:25:58 +0930

    FILACMS-27: Forms fixes

--- tests/Factories/FormFactory.php
+++ tests/Factories/FormFactory.php
@@ -2,8 +2,21 @@
 
 namespace Portable\FilaCms\Tests\Factories;
 
+use Filament\Forms\Components\CheckboxList;
+use Filament\Forms\Components\DatePicker;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Portable\FilaCms\Filament\FormBlocks\CheckboxBlock;
+use Portable\FilaCms\Filament\FormBlocks\CheckboxListBlock;
+use Portable\FilaCms\Filament\FormBlocks\DateTimeInputBlock;
+use Portable\FilaCms\Filament\FormBlocks\InformationBlock;
+use Portable\FilaCms\Filament\FormBlocks\RadioBlock;
+use Portable\FilaCms\Filament\FormBlocks\RelationshipBlock;
+use Portable\FilaCms\Filament\FormBlocks\RichTextBlock;
+use Portable\FilaCms\Filament\FormBlocks\SelectBlock;
+use Portable\FilaCms\Filament\FormBlocks\TextAreaBlock;
+use Portable\FilaCms\Filament\FormBlocks\TextInputBlock;
+use Portable\FilaCms\Filament\Resources\PageResource;
 use Portable\FilaCms\Models\Form;
 
 class FormFactory extends Factory
@@ -23,28 +36,126 @@ class FormFactory extends Factory
             'slug'      => Str::slug($title),
             'confirmation_title'  => 'Thank You!',
             'confirmation_text'    => tiptap_converter()->asJSON('<p>Thank you for submitting the form.</p>'),
-            'fields' => [
-                [
-                    "type" => "Text Field",
-                    "data" => [
-                        "field_name" => "Your Name",
-                        "text_type" => "text",
-                        "required" => true,
-                        "max_length" => null,
-                        "default_value" => null
-                    ]
-                ],
-                [
-                    "type" => "Text Field",
-                    "data" => [
-                        "field_name" => "Telephone",
-                        "text_type" => "telephone",
-                        "required" => true,
-                        "max_length" => null,
-                        "default_value" => null
+            'fields' => static::getFields()
+        ];
+    }
+
+    public static function getFields()
+    {
+        return [
+            [
+                "type" => CheckboxBlock::getBlockName(),
+                "data" => [
+                    "field_name" => "Checkbox",
+                    "required" => true,
+                    "default_value" => null
+                ]
+            ],
+            [
+                "type" => CheckboxListBlock::getBlockName(),
+                "data" => [
+                    "field_name" => "Checkbox List",
+                    "required" => true,
+                    "default_value" => null,
+                    "options" => [
+                        ["option_name" => "Option 1", "option_value" => "Option 1"],
+                        ["option_name" => "Option 2", "option_value" => "Option 2"],
+                        ["option_name" => "Option 3", "option_value" => "Option 3"],
                     ]
                 ]
-            ]
+            ],
+            [
+                "type" => DateTimeInputBlock::getBlockName(),
+                "data" => [
+                    "field_name" => "Date Picker",
+                    "required" => true,
+                    'date_type' => DatePicker::class,
+                    "default_value" => null
+                ]
+            ],
+            [
+                "type" => InformationBlock::getBlockName(),
+                "data" => [
+                    "field_name" => "Information",
+                    "contents" => [
+                        "type" => "paragraph",
+                        "attrs" => [
+                            "class" => null,
+                            "style" => null,
+                            "textAlign" => "start"
+                        ],
+                        "content" => [
+                            [
+                                "type" => "text",
+                                "text" => "This is an information block."
+                            ]
+                        ]
+                    ]
+                ]
+            ],
+            [
+                "type" => RadioBlock::getBlockName(),
+                "data" => [
+                    "field_name" => "Radio",
+                    "required" => true,
+                    "default_value" => null,
+                    "options" => [
+                        ["option_name" => "Option 1", "option_value" => "Option 1"],
+                        ["option_name" => "Option 2", "option_value" => "Option 2"],
+                        ["option_name" => "Option 3", "option_value" => "Option 3"],
+                    ]
+                ]
+            ],
+            [
+                "type" => RelationshipBlock::getBlockName(),
+                "data" => [
+                    "field_name" => "Relationship",
+                    "required" => true,
+                    "component_class" => CheckboxList::class,
+                    'relationship' => PageResource::class,
+                ]
+            ],
+            [
+                "type" => RichTextBlock::getBlockName(),
+                "data" => [
+                    "max_length" => 100,
+                    "field_name" => "Rich Text",
+                    "required" => true,
+                    "default_value" => null
+                ]
+            ],
+            [
+                "type" => SelectBlock::getBlockName(),
+                "data" => [
+                    "field_name" => "Select",
+                    "required" => true,
+                    "default_value" => null,
+                    "options" => [
+                        ["option_name" => "Option 1", "option_value" => "Option 1"],
+                        ["option_name" => "Option 2", "option_value" => "Option 2"],
+                        ["option_name" => "Option 3", "option_value" => "Option 3"],
+                    ]
+                ]
+            ],
+            [
+                "type" => TextAreaBlock::getBlockName(),
+                "data" => [
+                    "field_name" => "Text Area",
+                    "required" => true,
+                    "max_length" => 100,
+                    "default_value" => null
+                ]
+            ],
+            [
+                "type" => TextInputBlock::getBlockName(),
+                "data" => [
+                    "field_name" => "Text Input",
+                    "text_type" => "text",
+                    "required" => true,
+                    "max_length" => 100,
+                    "default_value" => null
+                ]
+            ],
         ];
     }
 }
