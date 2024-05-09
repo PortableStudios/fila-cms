b117261c1af06fd6d84945273478116b79bd569c
Kath Young <kath.young@portable.com.au>
2024-05-09 11:20:05 +0930

    Refactor image picker to use proper actions

--- src/Filament/Forms/Components/ImagePicker.php
+++ src/Filament/Forms/Components/ImagePicker.php
@@ -2,16 +2,28 @@
 
 namespace Portable\FilaCms\Filament\Forms\Components;
 
+use Filament\Actions\StaticAction;
+use Filament\Forms\Components\Actions\Action;
+use Filament\Forms\Components\Concerns\HasExtraItemActions as ConcernsHasExtraItemActions;
+use Filament\Forms\Components\Contracts\HasExtraItemActions;
 use Filament\Forms\Components\Field;
 use Portable\FilaCms\Models\Media;
 
-class ImagePicker extends Field
+class ImagePicker extends Field implements HasExtraItemActions
 {
-    protected string $view = 'fila-cms::filament.forms.components.image-picker';
-    public ?int $currentFolder;
-    public ?int $currentFile;
+    use ConcernsHasExtraItemActions;
 
+    protected string $view = 'fila-cms::filament.forms.components.image-picker';
     protected ?bool $isLive = true;
+
+    protected function setUp(): void
+    {
+        $this->registerActions([
+            fn (ImagePicker $component): Action => $component->pickImageAction(),
+            fn (ImagePicker $component): Action => $component->clearImageAction(),
+        ]);
+    }
+
 
     public function hasImage()
     {
@@ -26,5 +38,44 @@ class ImagePicker extends Field
             $model = Media::find($model);
         }
         return $model ? $model->small_thumbnail : Media::uploadImage();
+    }
+
+    public function clearImageAction()
+    {
+        return Action::make('clear-image')
+            ->action(function () {
+                $this->state(null);
+            });
+    }
+
+    public function pickImageAction()
+    {
+        return Action::make('pick-image')
+            ->form(
+                [
+                    MediaTable::make('selected-image')->afterStateUpdated(function ($state) {
+                        $livewire = $this->getLivewire();
+                        data_set($livewire, $this->getStatePath() . '_current_selection', $state);
+                    })
+                ]
+            )
+            ->modalSubmitAction(function (StaticAction $action) {
+                return $action->disabled(function () {
+                    $livewire = $this->getLivewire();
+                    $currentFile = data_get($livewire, $this->getStatePath() . '_current_selection');
+
+                    if(!$currentFile) {
+                        return true;
+                    }
+
+                    $model = Media::find($currentFile);
+                    return (!$model || !$model->is_image);
+                });
+            })->action(function () {
+                $livewire = $this->getLivewire();
+                $currentFile = data_get($livewire, $this->getStatePath() . '_current_selection');
+                $this->state($currentFile);
+            });
+        ;
     }
 }
