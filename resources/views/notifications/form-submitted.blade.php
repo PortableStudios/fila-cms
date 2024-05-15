17cec2776f76baad4cd9ff5f8b2fe7691c3844b5
Kath Young <kath.young@portable.com.au>
2024-05-16 08:14:27 +0930

    Correct form layout

--- resources/views/notifications/form-submitted.blade.php
+++ resources/views/notifications/form-submitted.blade.php
@@ -1,7 +1,7 @@
-<x-fila-cms::layouts.email>
+<fila-cms::layouts.email>
     <p>You've got a new form submission.</p>
 
     {!! $entry->display_html !!}
 
     <a href="{{ route('filament.admin.resources.forms.edit', $entry->form->id) }}">View Form</a>
-</x-fila-cms::layouts.email>
+</fila-cms::layouts.email>
