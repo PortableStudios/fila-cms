fed04935d911207a46d2d76da39c551cd4f9b044
Josh Young <jny986@gmail.com>
2024-06-07 07:56:17 +0930

    VG-247 Apply try catch to setting lookup to catch any issues allowing application to continue

--- src/Models/Setting.php
+++ src/Models/Setting.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Cache;
 use Portable\FilaCms\Observers\SettingObserver;
+use Throwable;
 
 #[ObservedBy(SettingObserver::class)]
 class Setting extends Model
@@ -18,12 +19,17 @@ class Setting extends Model
 
     public static function get($key)
     {
-        return Cache::rememberForever(
-            'setting-' . $key,
-            function () use ($key) {
-                return self::where('key', $key)->first()?->value;
-            }
-        );
+        try {
+            return Cache::rememberForever(
+                'setting-' . $key,
+                function () use ($key) {
+                    return self::where('key', $key)->first()?->value;
+                }
+            );
+        } catch (Throwable $e) {
+            report($e);
+            return null;
+        }
     }
 
     // Attibutes
