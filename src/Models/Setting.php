579fdc96b2cac130b5159d4fb7e3a6e1e22a9469
Josh Young <jny986@gmail.com>
2024-06-06 14:57:29 +0930

    VG-247 Addition of caching for Settings including observer to forget cached values

--- src/Models/Setting.php
+++ src/Models/Setting.php
@@ -2,17 +2,35 @@
 
 namespace Portable\FilaCms\Models;
 
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Cache;
+use Portable\FilaCms\Observers\SettingObserver;
 
+#[ObservedBy(SettingObserver::class)]
 class Setting extends Model
 {
+    protected string $cacheKey;
+
     protected $fillable = [
         'key',
         'value'
     ];
 
+    public function __construct(array $attributes = [])
+    {
+        parent::__construct($attributes);
+
+        $this->cacheKey = 'setting-' . $this->key;
+    }
+
     public static function get($key)
     {
-        return self::where('key', $key)->first()?->value;
+        return Cache::rememberForever(
+            'setting-' . $key,
+            function () use ($key) {
+                return self::where('key', $key)->first()?->value;
+            }
+        );
     }
 }
