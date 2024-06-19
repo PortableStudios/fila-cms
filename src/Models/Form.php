f5d78c7770b744b25940fc6f2264382093cc2719
Kath Young <kath.young@portable.com.au>
2024-06-19 20:17:43 +0930

    Sitemap fix

--- src/Models/Form.php
+++ src/Models/Form.php
@@ -2,6 +2,7 @@
 
 namespace Portable\FilaCms\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Notifications\Notifiable;
