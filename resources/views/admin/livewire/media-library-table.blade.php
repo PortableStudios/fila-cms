4bfbd4536c991792d269ba42aded0907e79df950
Jeremy Layson <jeremy.b.layson@gmail.com>
2024-06-20 19:39:55 +0800

    VG-327 Remove Dark Mode Styles

--- resources/views/admin/livewire/media-library-table.blade.php
+++ resources/views/admin/livewire/media-library-table.blade.php
@@ -3,13 +3,13 @@
         @foreach ($this->breadcrumbs() as $link => $breadcrumb)
       <li class="flex fi-breadcrumbs-item gap-x-2">
                 @if (!$loop->first)
-          <svg class="flex w-5 h-5 text-gray-400 fi-breadcrumbs-item-separator dark:text-gray-500 rtl:hidden"
+          <svg class="flex w-5 h-5 text-gray-400 fi-breadcrumbs-item-separator rtl:hidden"
             xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
                         <path fill-rule="evenodd"
                             d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z"
                             clip-rule="evenodd"></path>
                     </svg>
-          <svg class="flex w-5 h-5 text-gray-400 fi-breadcrumbs-item-separator dark:text-gray-500 ltr:hidden"
+          <svg class="flex w-5 h-5 text-gray-400 fi-breadcrumbs-item-separator ltr:hidden"
                         xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"
                         data-slot="icon">
                         <path fill-rule="evenodd"
@@ -18,7 +18,7 @@
                     </svg>
                 @endif
                 <a href="javascript:setParent('{{ $link }}')"
-          class="text-sm font-medium text-gray-500 transition duration-75 fi-breadcrumbs-item-label hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200">
+          class="text-sm font-medium text-gray-500 transition duration-75 fi-breadcrumbs-item-label hover:text-gray-700">
                     {{ $breadcrumb }}
                 </a>
             </li>
--- resources/views/tables/columns/roles-user.blade.php
+++ resources/views/tables/columns/roles-user.blade.php
@@ -1,7 +1,7 @@
 <div class="fi-ta-text grid w-full gap-y-1 px-3 py-4">
   <div class="flex">
     <div class="fi-ta-text-item inline-flex items-center gap-1.5 ">
-      <span class="fi-ta-text-item-label text-sm leading-6 text-gray-950 dark:text-white">
+      <span class="fi-ta-text-item-label text-sm leading-6 text-gray-950">
         {{ $getRecord()->name ?? $getRecord()->first_name }}
       </span>
     </div>
