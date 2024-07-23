1490dfd32b8454436ac210bd331132a7c35b3172
Kath Young <kath.young@portable.com.au>
2024-07-23 17:54:06 +0930

    Add configs for all commands

--- src/Listeners/CommandStartingListener.php
+++ src/Listeners/CommandStartingListener.php
@@ -10,7 +10,7 @@ class CommandStartingListener
 {
     public function handle(CommandStarting $event): void
     {
-        $indexCommands = ['scout:sync-index-settings','tinker'];
+        $indexCommands = ['scout:sync-index-settings','tinker','fila-cms:sync-search'];
         if(in_array($event->command, $indexCommands)) {
             FilaCms::setMeilisearchConfigs();
             BeforeSyncSearchSettings::dispatch();
