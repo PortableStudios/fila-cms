5a8ee67458913d4e346b7619f0f08056770a627e
Kath Young <kath.young@portable.com.au>
2024-07-24 09:30:20 +0930

    Update health check migration

--- database/migrations/2024_07_24_000000_create_health_tables.php
+++ database/migrations/2024_07_24_000000_create_health_tables.php
@@ -3,16 +3,14 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Spatie\Health\Models\HealthCheckResultHistoryItem;
 use Spatie\Health\ResultStores\EloquentHealthResultStore;
 
 return new class () extends Migration {
     public function up()
     {
-        $connection = (new HealthCheckResultHistoryItem())->getConnectionName();
         $tableName = EloquentHealthResultStore::getHistoryItemInstance()->getTable();
 
-        Schema::connection($connection)->create($tableName, function (Blueprint $table) {
+        Schema::create($tableName, function (Blueprint $table) {
             $table->id();
 
             $table->string('check_name');
@@ -27,7 +25,7 @@ return new class () extends Migration {
             $table->timestamps();
         });
 
-        Schema::connection($connection)->table($tableName, function (Blueprint $table) {
+        Schema::table($tableName, function (Blueprint $table) {
             $table->index('created_at');
             $table->index('batch');
         });
