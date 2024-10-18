dc2faad23c34031fa83716fc3d1ed72c41d3e7c9
Kath Young <kath.young@portable.com.au>
2024-10-18 11:15:29 +1030

    Adjust migration offsets

--- src/Providers/FilaCmsServiceProvider.php
+++ src/Providers/FilaCmsServiceProvider.php
@@ -223,13 +223,16 @@ class FilaCmsServiceProvider extends ServiceProvider
             __DIR__ . '/../../config/fila-cms.php' => config_path('fila-cms.php'),
         ], 'fila-cms-config');
 
-        // Get all the migration stubs and publish them
-        $filePath = __DIR__.'/../../stubs/database/migrations/auto*.stub';
-        $files = glob($filePath);
-        foreach ($files as $file) {
-            $this->publishes([
-                $file => $this->getMigrationFileName(basename($file, '.stub') . '.php'),
-            ], 'fila-cms-migrations');
+        // Only publish migrations if we're running inthe cli
+        if ($this->app->runningInConsole()) {
+            // Get all the migration stubs and publish them
+            $filePath = __DIR__.'/../../stubs/database/migrations/auto*.stub';
+            $files = glob($filePath);
+            foreach ($files as $file) {
+                $this->publishes([
+                    $file => $this->getMigrationFileName(basename($file, '.stub') . '.php'),
+                ], 'fila-cms-migrations');
+            }
         }
 
         // use the vendor configuration file as fallback
@@ -516,10 +519,13 @@ class FilaCmsServiceProvider extends ServiceProvider
      */
     protected function getMigrationFileName(string $migrationFileName): string
     {
-        $timestamp = date('Y_m_d_His');
+        // Offset the timestamp by the migration order, to make sure everything stays sequential
+        $offset = preg_match("/auto\_([0-9])+\_/", $migrationFileName, $matches) ? $matches[1] : 0;
+
+        $timestamp = date('Y_m_d_His', strtotime("+$offset seconds"));
 
         // Remove the auto_ prefix from the migration file name
-        $migrationFileName = preg_replace("/auto\_[0-9]+\_/", "", $migrationFileName);
+        $migrationFileName = preg_replace("/auto\_([0-9])+\_/", "", $migrationFileName);
 
         $filesystem = $this->app->make(Filesystem::class);
 
